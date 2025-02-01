3666b4daf4cf04574a7d339037dd3f912f0ba989
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-02-01 14:22:40 +0200

    init js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './main.js',
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
